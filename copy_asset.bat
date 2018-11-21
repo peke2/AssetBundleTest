980ac3ab0e3ff197b2df77edcc4620484451df09
Masaki Abe <abem@dc4.so-net.ne.jp>
2018-11-21 19:54:35 +0900

    作成済みの確認用リモート設置アセットバンドルをコピーするため、バッチを更新。 リモートサーバーの設置場所が環境に依存するためパスが直書き。他の場所で動かす場合は要検討。

--- copy_asset.bat
+++ copy_asset.bat
@@ -1,4 +1,5 @@
 @echo off
-copy Assets\AssetBundles\text Assets\StreamingAssets
+robocopy Assets\AssetBundles\json\local Assets\StreamingAssets\local /MIR
+robocopy Assets\AssetBundles\json\remote e:\develop\servers\test\asset\www\remote /MIR
 
 pause
