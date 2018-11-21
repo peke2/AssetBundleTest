484727e76ed619773a3dc3fa9d96c4e72c9fefb9
Masaki Abe <abem@dc4.so-net.ne.jp>
2018-11-21 12:04:51 +0900

    確認用にpersistentDataPathのディレクトリを開く項目をUnityエディターに追加。

---
+++ Assets/Editor/DirectoryUtil.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class DirectoryUtil
+{
+	[MenuItem("Tools/PersistentDataPathを開く")]
+	static void openPersistentDirectory()
+	{
+		System.Diagnostics.Process.Start(Application.persistentDataPath);
+	}
+}
