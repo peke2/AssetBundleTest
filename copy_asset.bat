e538272ddf043430a0076d4a8d4dde175dba1a79
Masaki Abe <abem@dc4.so-net.ne.jp>
2018-11-22 10:18:43 +0900

    リソースを確認用ディレクトリにコピーする際、metaファイルは除外するよう修正。 確認用にjson内のバージョンを変更。

--- copy_asset.bat
+++ copy_asset.bat
@@ -1,5 +1,5 @@
 @echo off
-robocopy Assets\AssetBundles\json\local Assets\StreamingAssets\local /MIR
-robocopy Assets\AssetBundles\json\remote %TEST_SERVER_PATH%\asset\www\remote /MIR
+robocopy Assets\AssetBundles\json\local Assets\StreamingAssets\local /MIR /xf *.meta
+robocopy Assets\AssetBundles\json\remote %TEST_SERVER_PATH%\asset\www\remote /MIR /xf *.meta
 
 pause
