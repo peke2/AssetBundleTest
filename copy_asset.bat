fb9df7a3b84d0b46b82f1391e9e5cda638fe8085
Masaki Abe <abem@dc4.so-net.ne.jp>
2018-11-22 10:12:35 +0900

    リモート用アセットのコピー先に環境変数を使うよう修正。 環境が変わっても同じようにバッチを使える。個人の確認用なので、ひとまずはこれで十分。

--- copy_asset.bat
+++ copy_asset.bat
@@ -1,5 +1,5 @@
 @echo off
 robocopy Assets\AssetBundles\json\local Assets\StreamingAssets\local /MIR
-robocopy Assets\AssetBundles\json\remote e:\develop\servers\test\asset\www\remote /MIR
+robocopy Assets\AssetBundles\json\remote %TEST_SERVER_PATH%\asset\www\remote /MIR
 
 pause
